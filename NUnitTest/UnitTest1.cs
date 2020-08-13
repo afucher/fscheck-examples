488950339987e215e671ce6535909a3c2f088a8c
Arthur Fücher <arthurfucher@gmail.com>
2020-08-13 19:56:50 -0300

    exemplos basicos com NUnit

---
+++ NUnitTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+using FsCheck;
+using NUnit.Framework;
+using static csharp_examples.Operacoes;
+
+namespace NUnitTest
+{
+    public class Tests
+    {
+
+        [Test]
+        public void SomaDoisNumeros()
+        {
+            Assert.AreEqual(5, Soma(2, 3));
+        }
+
+
+
+        [TestCase(5, 2, 3)]
+        [TestCase(5, 8, -3)]
+        public void SomaAlgunsDoisNumeros(int esperado, int numero1, int numero2)
+        {
+            Assert.AreEqual(esperado, Soma(numero1, numero2));
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property SomaVariosDoisNumeros(int numero1, int numero2)
+        {
+            var resultado = Soma(numero1, numero2);
+            return ((numero1 + numero2) == resultado).ToProperty();
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property SomaDoisNumerosN„oDependeDaOrdemDoPar‚metro(int numero1, int numero2)
+        {
+            var resultado1 = Soma(numero1, numero2);
+            var resultado2 = Soma(numero2, numero1);
+            return (resultado1 == resultado2).ToProperty();
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property SomaDoisNumerosIguaisDeveSerIgualAoDobroDoNumero(int numero1)
+        {
+            var resultado1 = Soma(numero1, numero1);
+            return (resultado1 == (numero1 * 2)).ToProperty();
+        }
+
+        [FsCheck.NUnit.Property]
+        public Property SomaDoisNumerosQuandoUm…ZeroDeveRetornarOValorPassado(int numero1)
+        {
+            var resultado1 = Soma(numero1, 0);
+            return (resultado1 == numero1).ToProperty();
+        }
+    }
+}
---
+++ csharp-examples/Operacoes.cs
@@ -0,0 +1,10 @@
+﻿namespace csharp_examples
+{
+    public static class Operacoes
+    {
+        public static int Soma(int numero1, int numero2)
+        {
+            return numero1 + numero2;
+        }
+    }
+}
